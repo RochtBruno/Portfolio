43e7b7bffbbd0cf2d8cfb8d02eca569a1e4288a0
Bruno <bruno.taveira.r0.382@gmail.com>
2025-07-31 11:50:29 -0300

    feat: experience section added

--- src/components/Exp/Exp.jsx
+++ src/components/Exp/Exp.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
 function Exp() {
-	
+	return(
+		<>
+			<div className="experience">
+				<h2 className="experience__title">Experience</h2>
+				<div className="experience__wrapper">
+					<div className="experience__box-date">
+						<p>2023 - Present</p>
+					</div>
+					<div className="experience__box-info">
+						<p className="experience__box-job">Full stack developer - <span>42 rio</span></p>
+						<p className="experience__box-job-description">Experiência como desenvolvedor full stack no programa 42 Labs 
+							fundamental para o meu desenvolvimento, proporcionando contato direto com problemas reais de empresas parceiras. 
+							Através desse programa, pude aprimorar minhas hard skills em desenvolvimento de software (incluindo React.js, Node.js, 
+							JavaScript/TypeScript, Python) e fortalecer minhas soft skills, atuando na resolução de problemas complexos. A vivência 
+							com desafios que impactam diretamente negócios e empregos foi um passo crucial na minha carreira, preparando-me para o 
+							mercado de trabalho em Engenharia de Software e desenvolvimento web full stack.</p>
+						<div className="experience__box-job-skills">
+							<p className="experience__box-job-skill">HTML</p>
+							<p className="experience__box-job-skill">HTML</p>
+							<p className="experience__box-job-skill">HTML</p>
+							<p className="experience__box-job-skill">HTML</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Exp
